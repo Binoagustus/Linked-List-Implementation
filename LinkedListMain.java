a30dcdd1c1c4254cf93a3499b5c964ff3ca0c2de
Binoagustus <binoagustus@gmail.com>
2022-11-17 22:35:47 +0530

    insert 40 after 30 to the sequence 56,30 and 70

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -8,7 +8,7 @@ public class LinkedListMain {
 		list.append(30);
 		list.append(70);
 		System.out.println();
-		System.out.println("search position of 30 is "+list.search(30));
+		System.out.println("Element inserted link position "+ list.insertAfter(30, 40));
 		System.out.println();
 		list.print();
 	}
